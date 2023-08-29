ef32e507c48a03d4807ff4bbc91cdade41b255c8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-29 09:31:42 +0300

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <div className='container'>
       <h1>Pasikartojimas</h1>
-      <Counter />
+      <Counter name='Push ups' />
+      <Counter name='Km ran' starting={5} />
     </div>
   );
 }
